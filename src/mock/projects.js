460ce2a940bdf36d31f43131c2451e70cdc892ba
Ruggyy <rugvedshindecr7@gmail.com>
2024-10-14 21:36:04 -0400

    Update projects.js

--- src/mock/projects.js
+++ src/mock/projects.js
@@ -55,7 +55,7 @@ export const PROJECTS = [
     description:
       'Simple and classic modern portfolio for who wants to present himself as a software developer. Portfolio build with highly customizable using Tailwind CSS, Next Js.',
     repoLink: '',
-    sourceLink: 'https://dhavalcode.com/',
+    sourceLink: '',
     category: [PROJECT_CATEGORY.OPEN_SOURCE],
   },
   {
